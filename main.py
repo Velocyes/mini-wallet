12cc4057e60f8886e860a4ed360e7bdbcc4c6f45
Andreas <Andreas.y45z@gmail.com>
2025-01-22 14:18:36 +0700

    feat(api): Adding endpoint to enable wallet

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 customers = {}
+wallets = {}
 
 SECRET_KEY = "JULO"
 
@@ -37,6 +38,27 @@ def initialize_account():
     
     customers[customer_xid] = {"wallet_id": None}
     return jsonify({"status": "success", "data": {"token": token}}), 201
+
+@app.route("/api/v1/wallet", methods=["POST"])
+def enable_wallet():
+    token = request.headers.get("Authorization", "").replace("Token ", "")
+    if not token or not _authenticate(token):
+        return jsonify({"status": "fail", "data": {"error": "Unauthorized"}}), 401
+    
+    customer_id = decode_jwt_token(token)
+    wallet_id = customers[customer_id].get("wallet_id")
+
+    if wallet_id and wallets[wallet_id]["status"] == "enabled":
+        return jsonify({"status": "fail", "data": {"error": "Already enabled"}}), 400
+
+    if not wallet_id:
+        wallet_id = uuid.uuid4().hex
+        customers[customer_id]["wallet_id"] = wallet_id
+        
+    enabled_at = datetime.utcnow().isoformat()
+
+    wallets[wallet_id] = {"id": wallet_id, "owned_by": customer_id, "status": "enabled", "enabled_at": enabled_at, "balance": 0}
+    return jsonify({"status": "success", "data": {"wallet": wallets[wallet_id]}}), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
